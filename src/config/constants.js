bb20d6fa43cc423965e4b21b8c7ff3ea4afa0add
software engineer <86639276+TopTalent333@users.noreply.github.com>
2023-02-18 17:32:06 +0200

    update

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,12 +3,13 @@ import firebase from 'firebase';
 require('firebase/firestore');
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyC-644MhwqMhNojjhrAv_7e8v0gIRtLKtY",
+  authDomain: "react-calendar-de.firebaseapp.com",
+  projectId: "react-calendar-de",
+  storageBucket: "react-calendar-de.appspot.com",
+  messagingSenderId: "1067229699096",
+  appId: "1:1067229699096:web:17d20b4b17c927c536376d",
+  measurementId: "G-ZDE0TYBJKT"
 };
 
 firebase.initializeApp(config);
